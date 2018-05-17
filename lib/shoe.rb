d726158cad074207d943a5b46e0fac8f3862c04e
Eric Chrobak <eric@ericchrobak.com>
2018-05-17 01:19:42 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,12 +2,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRAND = []
+  BRANDS = []
   def initialize(brand)
     @brand = brand
 
-    unless BRAND.include?(brand)
-      BRAND << brand
+    unless BRANDS.include?(brand)
+      BRANDS << brand
     end
   end
 
